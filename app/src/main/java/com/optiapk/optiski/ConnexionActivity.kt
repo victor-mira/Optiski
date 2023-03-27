760ae22467fda281930db657a528d2c917a080c8
Bell <skyfall.kay.team@gmail.com>
2023-03-27 18:10:31 -0400

    inscription layout revision
    - séparation des layout d'inscription et de connexion
    - ajout d'un edit de confimation de password sur le layout d'inscription
    - ajout des string correspondantes aux ressources

--- app/src/main/java/com/optiapk/optiski/ConnexionActivity.kt
+++ app/src/main/java/com/optiapk/optiski/ConnexionActivity.kt
@@ -1,18 +1,30 @@
 package com.optiapk.optiski
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class ConnexionActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connexion)
         supportActionBar?.hide()
 
-        val buttonSignInGoogle = findViewById<Button>(R.id.signInGoogleButtonAlternate)
-        val buttonNext = findViewById<Button>(R.id.nextButton)
+        auth = FirebaseAuth.getInstance()
+
+        val buttonSignInGoogle = findViewById<Button>(R.id.signInGoogleButtonAlternateInscription)
+        val buttonNext = findViewById<Button>(R.id.nextButtonInscription)
+        val email = findViewById<EditText>(R.id.enterEmailInscription)
+        val password = findViewById<EditText>(R.id.enterPasswordInscription)
 
         buttonSignInGoogle.setOnClickListener {
             val intent = Intent(this, ChoicesActivity::class.java)
@@ -20,9 +32,39 @@ class ConnexionActivity : AppCompatActivity() {
         }
 
         buttonNext.setOnClickListener {
-            val intent = Intent(this, ChoicesActivity::class.java)
-            startActivity(intent)
+            signInWithPassword(email.text.toString(), password.text.toString())
+//            val intent = Intent(this, ChoicesActivity::class.java)
+//            startActivity(intent)
         }
 
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            // UI change
+        }
+    }
+
+    public fun signInWithPassword(email:String, password:String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+
+
 }
--- app/src/main/java/com/optiapk/optiski/InscriptionActivity.kt
+++ app/src/main/java/com/optiapk/optiski/InscriptionActivity.kt
@@ -1,20 +1,57 @@
 package com.optiapk.optiski
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class InscriptionActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inscription)
+        setContentView(R.layout.activity_inscription1)
         supportActionBar?.hide()
 
-        val buttonNext = findViewById<Button>(R.id.nextInscriptionButton)
+        auth = FirebaseAuth.getInstance()
+
+        val buttonNext = findViewById<Button>(R.id.nextButtonInscription)
+        val EditMail = findViewById<EditText>(R.id.enterEmailInscription)
+        val EditPassword = findViewById<EditText>(R.id.enterPasswordInscription)
+        val EditConfirmPassword = findViewById<EditText>(R.id.confirmPasswordInscription)
+        val buttonInscription = findViewById<Button>(R.id.inscriptionButton)
+
         buttonNext.setOnClickListener {
-            val intent = Intent(this, ConnexionActivity::class.java)
-            startActivity(intent)
+            setContentView(R.layout.activity_inscription2)
+//            val intent = Intent(this, ConnexionActivity::class.java)
+//            startActivity(intent)
         }
+
+
     }
+
+    public fun incriptionWithPassword(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(ContentValues.TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+            }
+    }
+
 }
