67bc767e1fae7825251605efafb157cbfe6bc853
MiniiiB <92688616+MiniiiB@users.noreply.github.com>
2023-04-12 14:06:13 -0400

    Ajout de l'écran de fin (Feedback)

--- app/src/main/java/com/optiapk/optiski/Feedback.kt
+++ app/src/main/java/com/optiapk/optiski/Feedback.kt
@@ -2,10 +2,19 @@ package com.optiapk.optiski
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.optiapk.optiski.models.Piste
+import org.w3c.dom.Text
 
 class Feedback : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback)
+        val nbrPistes = (1..5).random()
+        val duree = (30..90).random()
+
+        val textFinDescription: TextView = findViewById(R.id.textFinDescription)
+        textFinDescription.text = " Vous avez parcouru" + nbrPistes.toString() + "en" + duree.toString()
     }
+
 }
